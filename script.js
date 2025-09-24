61289712268bf961647a72774547ae78be89bf89
Sammmy1031 <85348081+Sammmy1031@users.noreply.github.com>
2025-09-24 14:26:06 +0900

    サイト更新１

--- script.js
+++ script.js
@@ -368,7 +368,7 @@ detailsPanel.appendChild(infoHeader);
     showLoading(true, "データを送信中...");
     
     try {
-        const gasWebAppUrl = 'https://script.google.com/macros/s/AKfycbzB474v5g3S9_tR6o_X6jiFdxS64keEFhV97AA7Jw5K5wOqF9eD1IDsQd-J8Sseni6syg/exec'; 
+        const gasWebAppUrl = 'https://script.google.com/macros/s/AKfycbz0bmNUp44bmRt6_HEC1kulC1SAcEhP7VljceEIT4uqrXfb5wA-ICiO2YN1WlPTYvsA/exec'; 
         const dataToSave = { ...experimentData };
         dataToSave.experimentEndTimeISO = new Date().toISOString();
         
